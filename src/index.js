fa15d560f33cc6f97de703664a73e453da482c61
vokur13 <vokur13@gmail.com>
2022-10-16 19:06:20 +0300

    project init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AuthProvider from './contexts/auth/Provider';
@@ -7,13 +7,13 @@ import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
